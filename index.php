b17f44fed339999f95db72ece5a30db6d34397c9
ned-dev-2022 <exaucengango20@gmail.com>
2022-05-27 19:51:29 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/style.css ">
+    <title>GOOD MEDIA</title>
+</head>
+<body>
+
+    <div class="container-fluid p-0 " id="good">
+        <div class="container-fluid py-5" id="media">
+            <div class="container">
+                <div class="row py-5">
+                    <div class="col-md-12 py-5">
+                        <h1 class="text-light">GOOD MEDIA<span class="text-danger">.</span></h1>
+                        <p class="text-light" style="max-width: 650px;">La vie est comme un appareil photo!
+                            Zoomez sur ce qui est important, capturez les bons moments, développer le meilleur et supprimez le reste, et si le résultat ne donne pas ce que vous souhaitez, recommencez!
+                        </p>
+                        <a class="btn btn-danger" href="#rdv">Prendre rendez-vous</a>
+                    </div>
+                </div>
+             </div>
+        </div>
+    </div>
+
+
+    <div class="container my-5">
+        <div class="row">
+            <div class="col-md-12 p-0">
+                <h4 class="bg-danger titre-section" class="titre-text">NOS SERVICE</h4>
+                <P>
+                    Mariage, Anniversaire, Evénement...
+                </P>
+                              
+            </div>
+        </div>
+
+        <div class="row mt-1">
+            <img src="assets/img/20fad19539eb7643fdccaef38f78b778.jpg" alt="nos services en images" class="col-md-6 p-0 img-services">
+            <img src="assets/img/clara.jpg" alt="nos services en images" class="col-md-6 p-0 img-services">
+        </div>
+
+        <div class="row">
+            <div class="col-md-12 py-3">
+                <h5 class="text-center">Services et catalogues</h5>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-3 block-service-1 p-0" style="background-image: url('./assets/img/shooting\ \(2\).jpg');">
+                <div class="services-div-rgba">
+                    <h5 class="text-light">paysage</h5>
+                </div>
+            </div>
+
+            <div class="col-md-3 block-service-1 p-0" style="background-image: url('./assets/img/mariage1.jpg');">
+                <div class="services-div-rgba">
+                    <h5 class="text-light">Mariage</h5>
+                </div>
+            </div>
+
+            <div class="col-md-3 block-service-1 p-0" style="background-image: url('./assets/img/fete.jpg');">
+                <div class="services-div-rgba">
+                    <h5 class="text-light">Anniversaire
+
+                    </h5>
+                </div>
+            </div>
+
+            <div class="col-md-3 block-service-1 p-0"style="background-image: url('./assets/img/lune.jpg');">
+                <div class="services-div-rgba">
+                    <h5 class="text-light">Mariage</h5>
+                </div>
+            </div>
+
+            <div class="col-md-3 block-service-1 p-0" style="background-image: url('./assets/img/nature\ \(2\).jpg');">
+                <div class="services-div-rgba">
+                    <h5 class="text-light">nature</h5>
+                </div>
+            </div>
+
+            <div class="col-md-3 block-service-1 p-0" style="background-image: url('./assets/img/plage.jpg');">
+                <div class="services-div-rgba">
+                    <h5 class="text-light">plage</h5>
+                </div>
+            </div>
+
+            <div class="col-md-3 block-service-1 p-0" style="background-image: url('./assets/img/animaux1.jpg');">
+                <div class="services-div-rgba">
+                    <h5 class="text-light">animaux</h5>
+                </div>
+            </div>
+
+            <div class="col-md-3 block-service-1 p-0" style="background-image: url('./assets/img/camera.jpg');">
+                <div class="services-div-rgba">
+                    <h5 class="text-light">camera</h5>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    
+    <div class="container" id="anna">
+        <div class="row">
+            <div class="col-md-12">
+                <h4 class="bg-danger titre-section"> NOS OEUVRES</h4>
+            </div>
+        </div>
+        <div class="row my-3">
+            <img src="assets/img/tati.jpg" class="col-md-3" alt="">
+            <img src="assets/img/eaux1.jpg" class="col-md-3" alt="">
+            <img src="assets/img/fleur.jpg" class="col-md-3" alt="">
+            <img src="assets/img/lune.jpg" class="col-md-3" alt="">
+        </div>
+
+        <div class="row my-3">
+            <img src="assets/img/mariage.jpg" class="col-md-3" alt="">
+            <img src="assets/img/portrait3.jpg" class="col-md-3" alt="">
+            <img src="assets/img/shooting (2).jpg" class="col-md-3" alt="">
+            <img src="assets/img/savane.jpg" class="col-md-3" alt="">
+        </div>
+    </div>
+
+    <div class="container my-5">
+        <div class="col-md-12">
+            <h4 class="bg-danger titre-section"id="rdv">Rendez-vous</h4>
+        </div>
+
+        <form class="row">
+            <div class="col-md-12 form-group my-2">
+                <input type="text" name="nom" placeholder="Nom complet" class="form-control">
+            </div>
+
+            <div class="col-md-6 form-group my-2">
+                <input type="email" name="nom" placeholder="Adresse electronique" class="form-control">
+            </div>
+
+        <div class="col-md-6 form-group my-2">
+                <input type="text" name="nom" placeholder="Numero de téléphone" class="form-control">
+            </div>
+
+            <div class="col-md-12 form-group my-2">
+                <label>Objet de votre rendez-vous !</label>
+                <select name="" id="" class="form-select">
+                    <option value="mariage">Mariage</option>
+                    <option value="paysage">paysage</option>
+                    <option value="Anniversaire">Anniversaire</option>
+                    <option value="nature">nature</option>
+                    <option value="plage">plage</option>
+                    <option value="animaux">animaux</option>
+                    <option value="portrait">portrait</option>
+                    <option value="camera">camera</option>
+                </select>
+            </div>
+
+            <div class="col-md-6 form-group my-2">
+                <input type="date" name="date" class="form-control">
+            </div>
+
+            <div class="col-md-6 form-group my-2">
+                <input type="time" name="nom" class="form-control">
+            </div>
+
+            <div class="col-md-12 form-group my-2">
+                <input type="submit" placeholder="Numero de téléphone" class="btn btn-danger col-12">
+            </div>
+
+
+        </form>
+    </div>
+
+</body>
+</html>
